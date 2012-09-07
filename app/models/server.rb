4d55f0669f7d88dca3e749bcc52e433255559181
Rajib Chowdhury <rajib.chowdhury@indusnet.co.in>
2012-09-07 17:52:59 +0530

    url validation fixed completely.

--- app/models/server.rb
+++ app/models/server.rb
@@ -3,7 +3,7 @@ class Server < ActiveRecord::Base
 
   validates :name, presence: true
   validates :url, presence: true, 
-            format: { :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix }
+            format: { :with => /^(http|https):\/\/([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}|(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}|localhost)(:[0-9]{1,5})?(\/.*)?$/ix }
 
   after_create :update_status
 
