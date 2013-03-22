221b572ecea71f55901d03aa23938326f5f3a2ac
Rajib Chowdhury <rajib.chowdhury@indusnet.co.in>
2013-03-22 12:43:38 +0530

    rails version updated to 3.2.13.rc2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails3BootstrapDeviseCancan::Application.config.session_store :cookie_store, key: '_rails3-bootstrap-devise-cancan_session'
+Rails3BootstrapDeviseCancan::Application.config.session_store :cookie_store, key: '_rails3_bootstrap_devise_cancan_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
