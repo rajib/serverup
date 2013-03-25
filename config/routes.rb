8626c991a2b4509b351e121fb357d76e470a10ce
Siddhartha <siddhartha@siddharthaBose611.(none)>
2013-03-25 12:59:59 +0530

    Configured routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails3BootstrapDeviseCancan::Application.routes.draw do
 
   devise_for :users
   resources :servers
+  resources :contacts
   resources :users, :only => [:show, :index]
+
+  resources :servers do
+    resources :contacts
+    resources :contacts_servers
+  end
+
+  resources :contacts_servers, only: [:assign]
 
 	namespace :api do
 		namespace :v1  do
