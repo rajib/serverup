267a56c4a9c0183c548dc56a8c3c850efb497007
Siddhartha <siddhartha@siddharthaBose611.(none)>
2013-03-28 10:50:13 +0530

    Already added users will not be shown while adding a user for a server

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,17 @@ class Contact < ActiveRecord::Base
   attr_accessible :email, :name
   attr_accessible :email, :name, :user_id
 
-  def self.populate_contacts(current_user)
-  	contacts = Contact.where('user_id=?',current_user.id).select([:name, :email, :id])
+  def self.populate_contacts(current_user, server_id)
+    server = Server.find server_id
+    
+    already_added_contact_ids = server.contacts.map {|c| c.id}
+    
+    if already_added_contact_ids.empty?
+      contacts = Contact.where('user_id=?',current_user.id).select([:name, :email, :id])
+    else
+  	  contacts = Contact.where('user_id=? and id NOT IN (?)',current_user.id, already_added_contact_ids).select([:name, :email, :id])
+    end
+    
     return contacts.to_json
   end
 end
