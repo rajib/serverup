21dd70015fc639138ee8403e4cb016353ceb3253
Siddhartha <siddhartha@siddharthaBose611.(none)>
2013-03-30 14:36:30 +0530

    Added typehead to implement autocomplete

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,11 +10,12 @@ class Contact < ActiveRecord::Base
     already_added_contact_ids = server.contacts.map {|c| c.id}
     
     if already_added_contact_ids.empty?
-      contacts = Contact.where('user_id=?',current_user.id).select([:name, :email, :id])
+       contacts = Contact.where('user_id=?',current_user.id).select([:name, :email, :id])
     else
-      contacts = Contact.where('user_id=? and id NOT IN (?)',current_user.id, already_added_contact_ids).select([:name, :email, :id])
+       contacts = Contact.where('user_id=? and id NOT IN (?)',current_user.id, already_added_contact_ids).select([:name, :email, :id])
     end
-    
-    return contacts.to_json
+  
+     return contacts.to_json
+
   end
 end
